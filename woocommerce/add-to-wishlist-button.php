4daf81ec14c1a5c2f38462b35feb872537c6a827
Anuj <stoneera111@gmail.com>
2019-11-21 14:20:14 +0545

    == 1.0.3 - 21 November, 2019 ==
    - Fix: Banner right side small images in mobile device
    - Fix: Post navigation in mobile device
    - Fix: Space after footer when header 2 is set.
    - Fix: Problem with tab in product detail page
    - Fix: Text alignment in table of wishlist page
    - Tweak: Quick view icon and wishlist icon when hovered on product
    - Fix: Issue with menu link description in header layout 2
    - Tweak: Colour of megamenu submenu item first child
    - Tweak: Design of store notice (WooCommerce default notice at customizer)

--- woocommerce/add-to-wishlist-button.php
+++ woocommerce/add-to-wishlist-button.php
@@ -14,5 +14,6 @@ if ( ! defined( 'YITH_WCWL' ) ) {
 global $product;
 ?>
 
-<a class="os-tooltip <?php echo esc_attr( $link_classes ); ?>" href="<?php echo esc_url( add_query_arg( 'add_to_wishlist', $product_id ) )?>" rel="nofollow" data-product-id="<?php echo esc_attr( $product_id ); ?>" data-product-type="<?php echo esc_attr( $product_type ); ?>" data-tippy-content="<?php esc_attr_e( 'Add to wishlist', 'orchid-store' ); ?>" ><i class="bx bx-heart"></i></a>
+<a class="button-general wish-list-button" href="<?php echo esc_url( add_query_arg( 'add_to_wishlist', $product_id ) )?>" rel="nofollow" data-product-id="<?php echo esc_attr( $product_id ); ?>" data-product-type="<?php echo esc_attr( $product_type ); ?>" ><span class="icon"><i class="fa fa-heart-o" aria-hidden="true"></i>
+</span> <span class="text"><?php echo __( 'Add to wishlist', 'orchid-store' ); ?></span></a>
 <img src="<?php echo esc_url( YITH_WCWL_URL . 'assets/images/wpspin_light.gif' ) ?>" class="ajax-loading" alt="loading" width="16" height="16" style="visibility:hidden" />
