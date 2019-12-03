3cfbdb578a8fd5f590c09e1e4bcf7408a7f6c9d2
Anuj <stoneera111@gmail.com>
2019-12-03 13:19:17 +0545

    Fix Quantity input buttons styles
    Fix Quantity input buttons styles

--- assets/src/js/custom/custom-scripts.js
+++ assets/src/js/custom/custom-scripts.js
@@ -152,40 +152,38 @@
         */
 
 
-        if( $('body').hasClass('single-product') ) {
+        $('form.cart').on('click', 'button.woo-quantity-plus, button.woo-quantity-minus', function() {
 
-            $('form.cart').on( 'click', 'button.plus, button.minus', function() {
-  
-                // Get current quantity values
-                var qty = $(this).closest( 'form.cart' ).find( '.qty' );
-                var val   = parseFloat(qty.val());
-                var max = parseFloat(qty.attr( 'max' ));
-                var min = parseFloat(qty.attr( 'min' ));
-                var step = parseFloat(qty.attr( 'step' ));
-      
-                // Change the value if plus or minus
-                if( $(this).is('.plus') ) {
+            // Get current quantity values
+            var qty = $(this).closest('form.cart').find('.qty');
+            var val = parseFloat(qty.val());
+            var max = parseFloat(qty.attr('max'));
+            var min = parseFloat(qty.attr('min'));
+            var step = parseFloat(qty.attr('step'));
 
-                    if( max && ( max <= val ) ) {
+            // Change the value if plus or minus
+            
+            if ($(this).is('.woo-quantity-plus')) {
 
-                        qty.val( max );
-                    } else {
+                if (max && (max <= val)) {
 
-                        qty.val( val + step );
-                    }
+                    qty.val(max);
                 } else {
 
-                    if( min && ( min >= val ) ) {
-
-                        qty.val( min );
-                    } else if( val > 1 ) {
-
-                        qty.val( val - step );
-                    }
+                    qty.val(val + step);
                 }
-                  
-            });
-        }
+            } else {
+
+                if (min && (min >= val)) {
+
+                    qty.val(min);
+                } else if (val > 1) {
+
+                    qty.val(val - step);
+                }
+            }
+
+        });
 
 
         /*
