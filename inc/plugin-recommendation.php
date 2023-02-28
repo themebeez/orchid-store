e8804e3d8f1d256f8aafe9678eb1c107626e6c51
Ashok Lama <crazyhat17@gmail.com>
2023-02-28 10:36:26 +0545

    Correct condition check for TGMPA plugin recommendation
    - Correct condition check for TGMPA plugin recommendation

--- inc/plugin-recommendation.php
+++ inc/plugin-recommendation.php
@@ -1,63 +1,66 @@
 <?php
-
-/*
- * Hook - Plugin Recommendation
+/**
+ * TGMPA plugins recommendation.
+ *
+ * @package Orchid_Store
  */
+
 if ( ! function_exists( 'orchid_store_recommended_plugins' ) ) :
-    /**
-     * Recommend plugins.
-     *
-     * @since 1.0.0
-     */
-    function orchid_store_recommended_plugins() {
+	/**
+	 * Recommend plugins.
+	 *
+	 * @since 1.0.0
+	 */
+	function orchid_store_recommended_plugins() {
 
-        $plugins = array(
-            array(
-                'name'     => 'Themebeez Toolkit',
-                'slug'     => 'themebeez-toolkit',
-                'required' => false,
-            ),
-            array(
-                'name'     => 'WooCommerce',
-                'slug'     => 'woocommerce',
-                'required' => false,
-            ),
-            array(
-                'name'     => 'Elementor Page Builder',
-                'slug'     => 'elementor',
-                'required' => false,
-            ),
-        );
+		$plugins = array(
+			array(
+				'name'     => 'Themebeez Toolkit',
+				'slug'     => 'themebeez-toolkit',
+				'required' => false,
+			),
+			array(
+				'name'     => 'WooCommerce',
+				'slug'     => 'woocommerce',
+				'required' => false,
+			),
+			array(
+				'name'     => 'Elementor Page Builder',
+				'slug'     => 'elementor',
+				'required' => false,
+			),
+		);
 
-        if ( class_exists( 'WooCommerce' ) ) {
+		if ( class_exists( 'WooCommerce' ) ) {
 
-            if ( ! class_exists( 'YITH_WCQV' ) ) {
+			if ( ! class_exists( 'Addonify_Quick_View' ) ) {
 
-                $plugins[] = array(
-                    'name'     => 'Addonify WooCommerce Quick View',
-                    'slug'     => 'addonify-quick-view',
-                    'required' => false,
-                );
-            }
+				$plugins[] = array(
+					'name'     => 'Addonify WooCommerce Quick View',
+					'slug'     => 'addonify-quick-view',
+					'required' => false,
+				);
+			}
 
-            if ( ! class_exists( 'YITH_WCWL_Wishlist' ) ) {
+			if ( ! class_exists( 'Addonify_Wishlist' ) ) {
 
-                $plugins[] = array(
-                    'name'     => 'Addonify WooCommerce Wishlist',
-                    'slug'     => 'addonify-wishlist',
-                    'required' => false,
-                );
-            }
+				$plugins[] = array(
+					'name'     => 'Addonify WooCommerce Wishlist',
+					'slug'     => 'addonify-wishlist',
+					'required' => false,
+				);
+			}
 
-            $plugins[] = array(
-                'name'     => 'Addonify – Compare Products For WooCommerce',
-                'slug'     => 'addonify-compare-products',
-                'required' => false,
-            );
-        }
+			$plugins[] = array(
+				'name'     => 'Addonify - Compare Products For WooCommerce',
+				'slug'     => 'addonify-compare-products',
+				'required' => false,
+			);
+		}
 
-        tgmpa( $plugins );
-    }
+		tgmpa( $plugins );
+	}
 
+	add_action( 'tgmpa_register', 'orchid_store_recommended_plugins' );
 endif;
-add_action( 'tgmpa_register', 'orchid_store_recommended_plugins' );
+
